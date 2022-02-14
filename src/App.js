fe74659099ea39fa1ff6025d44ea857348d7f958
Tate-T <tatjana.tarasovych@gmail.com>
2022-02-14 22:43:07 +0200

    main links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+// import HomePage from './components/HomePage/HomePage';
+// import MoviesPage from './components/MoviesPage/MoviesPage';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Link to="/homepage">HomePage</Link>
+      <Link to="/MoviesPage">MoviesPage</Link>
     </div>
   );
 }
