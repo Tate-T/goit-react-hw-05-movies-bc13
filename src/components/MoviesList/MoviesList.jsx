c676bbc115990267f7c688566479dffb1d19f9de
Tate-T <tatjana.tarasovych@gmail.com>
2022-02-16 22:03:20 +0200

    fixed MoviesDetailsView render

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,16 +1,22 @@
 import s from './MoviesList.module.css';
-import { Link } from 'react-router-dom';
+import { useLocation, NavLink } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
 const MoviesList = ({ movies }) => {
+    const location = useLocation();
+
     return (
         <ul className={s.moviesList} >
             {movies.map(movie =>
             (<li key={movie.id}>
-                <Link to=''>{movie.title || movie.name}</Link>
+                <NavLink
+                    to={{
+                        pathname: '/movies/' + movie.id,
+                        state: { from: location },
+                    }} activeStyle={{ color: "rgb(5, 153, 238)" }}>{movie.title || movie.name}</NavLink>
             </li>)
             )}
-        </ul>
+        </ul >
     )
 }
 
--- src/views/MovieDetailsView.js
+++ src/views/MovieDetailsView.js
@@ -1,16 +1,16 @@
 import { useState, useEffect } from 'react';
-import { useParams, Route, Link } from "react-router-dom";
-
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 import fetches from '../services/moviesAPI';
+// import Cast from '../components/Cast/Cast';
+// import Reviews from '../components/Reviews/Reviews';
 
 export default function MoviesDetailsView() {
+    const match = useRouteMatch();
     const { movieId } = useParams();
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-        fetches.fetchMovieDetail(movieId).then(res => {
-            setMovies(res.movies)
-        });
+        fetches.fetchMovieDetail(movieId).then(res => setMovies(res));
     }, [movieId, movies])
 
 
@@ -18,28 +18,33 @@ export default function MoviesDetailsView() {
         <>
             {movies && <>
                 <div>
-                    {movies.poster_path}
+                    <Link to="/">Go back</Link>
+                    <img src={movies.poster_path} alt='' />
                     <h2>{movies.title}</h2>
                     <p>User score:{movies.vote_average}</p>
                     <h3>Overview</h3>
                     <p>{movies.overview}</p>
                     <h4>Genres</h4>
-                    <p>{movies.genres.genre.name}</p>
+                    <ul>
+                        {movies.genres && movies.genres.map(genre => (
+                            <li key={genre.id}>{genre.name}</li>
+                        ))}
+                    </ul>
                 </div>
                 <div>
                     <h2>Additional information</h2>
-                    <Link to=''>Cast</Link>
-                    <Link to=''>Reviews</Link>
+                    <Link to={match.url + '/cast'}>Cast</Link>
+                    <Link to={match.url + '/reviews'}>Reviews</Link>
                 </div>
             </>
             }
 
-            <Route path="/movies/:movieId/cast">
-                {/* <CastView /> */}
+            <Route path={match.url + '/cast'}>
+                {/* <Cast /> */}
             </Route>
 
-            <Route path="/movies/:movieId/reviews">
-                {/* <ReviewsView /> */}
+            <Route path={match.url + '/reviews'}>
+                {/* <Reviews /> */}
             </Route>
         </>
     )
