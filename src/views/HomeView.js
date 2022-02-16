f5a9fd71510799f8e4c90bae7b1d82f9ea3662ad
Tate-T <tatjana.tarasovych@gmail.com>
2022-02-16 12:12:31 +0200

    movies.id

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,10 +1,11 @@
 import HomePage from '../components/HomePage/HomePage';
+import { Link } from 'react-router-dom';
 
 export default function HomeView(movies) {
     return (
         <>
             <HomePage />
-            <ul>
+            <ul key={movies.id}>
                 {movies.map(movie => {
                     <li>
                         <Link>{movie}</Link>
