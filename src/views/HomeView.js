316ffe4140945f4b42f3c6d94184c7338e2bd6ac
Tate-T <tatjana.tarasovych@gmail.com>
2022-02-16 12:18:49 +0200

    PropTypes ins

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -1,17 +1,21 @@
 import HomePage from '../components/HomePage/HomePage';
 import { Link } from 'react-router-dom';
-
-export default function HomeView(movies) {
+import PropTypes from 'prop-types';
+export default function HomeView({ movies }) {
     return (
         <>
             <HomePage />
             <ul key={movies.id}>
-                {movies.map(movie => {
-                    <li>
-                        <Link>{movie}</Link>
-                    </li>
-                })}
+                {movies.map(movie =>
+                (<li>
+                    <Link>{movie}</Link>
+                </li>)
+                )}
             </ul>
         </>
     )
+}
+
+HomePage.PropTypes = {
+    movies: PropTypes.func
 }
