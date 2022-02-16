ca92f5857e1dac849182a9c456697be7b3ff630a
Tate-T <tatjana.tarasovych@gmail.com>
2022-02-16 23:21:17 +0200

    MoviesDetailsView styles

--- src/views/MovieDetailsView.js
+++ src/views/MovieDetailsView.js
@@ -18,23 +18,49 @@ export default function MoviesDetailsView() {
         <>
             {movies && <>
                 <div>
-                    <Link to="/">Go back</Link>
-                    <img src={movies.poster_path} alt='' />
-                    <h2>{movies.title}</h2>
-                    <p>User score:{movies.vote_average}</p>
-                    <h3>Overview</h3>
-                    <p>{movies.overview}</p>
-                    <h4>Genres</h4>
-                    <ul>
-                        {movies.genres && movies.genres.map(genre => (
-                            <li key={genre.id}>{genre.name}</li>
-                        ))}
-                    </ul>
+                    <Link to="/" style={{
+                        display: 'block',
+                        width: '100px',
+                        margin: '20px',
+                        padding: '10px',
+                        backgroundColor: 'rgb(5, 153, 238)',
+                        border: '1px solid black',
+                        borderRadius: '5px',
+                        textAlign: 'center',
+                        alignItems: 'center',
+                        color: 'white',
+                        textDecoration: 'none'
+                    }}>
+                        Go back</Link>
+                    <div style={{ display: 'flex' }}>
+                        <img src={`https://image.tmdb.org/t/p/w300${movies.poster_path}`} style={{ marginLeft: '20px', marginRight: '20px' }} alt='' />
+                        <div style={{ margin: '20px' }}>
+                            <h2>{movies.title}</h2>
+                            <p>User score: {movies.vote_average}</p>
+                            <h3>Overview</h3>
+                            <p>{movies.overview}</p>
+                            <h4>Genres:</h4>
+                            <ul>
+                                {movies.genres && movies.genres.map(genre => (
+                                    <li key={genre.id}>{genre.name}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <h2>Additional information</h2>
-                    <Link to={match.url + '/cast'}>Cast</Link>
-                    <Link to={match.url + '/reviews'}>Reviews</Link>
+                <div style={{
+                    margin: '20px',
+                    borderBottom: '1px solid grey',
+                    borderTop: '1px solid grey',
+                    boxShadow: '0px 0px 4px blue'
+                }}>
+                    <h2 style={{
+                        marginLeft: '20px'
+                    }}>Additional information</h2>
+                    <div>
+                        <Link to={match.url + '/cast'} style={{ display: 'block', margin: '20px' }}>Cast</Link>
+                        <Link to={match.url + '/reviews'} style={{ display: 'block', margin: '20px' }}>Reviews</Link>
+                    </div>
                 </div>
             </>
             }
